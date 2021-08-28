2b7506b6a0242f5dba2657bbc2af5d4113e18bb9
yallu201 <dlruddlf07@gmail.com>
2021-08-28 09:15:00 +0900

    Create directory 'lib' for common logic

---
+++ src/lib/createRequestThunk.js
@@ -0,0 +1,28 @@
+import { startLoading, finishLoading } from '../modules/loading.js';
+
+function createRequestThunk(type, request) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+  return params => async dispatch => {
+    dispatch({ type });
+    dispatch(startLoading(type));
+    try {
+      const response = await request(params);
+      console.log(response);
+      dispatch({
+        type: SUCCESS,
+        payload: response.data,
+      });
+    } catch (e) {
+      dispatch({
+        type: FAILURE,
+        payload: e,
+        error: true,
+      });
+      throw e;
+    } finally {
+      dispatch(finishLoading(type));
+    }
+  };
+}
+export default createRequestThunk;
