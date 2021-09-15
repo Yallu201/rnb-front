f8be233c731bd576687075f908b65c90dcaff7e5
yallu201 <dlruddlf07@gmail.com>
2021-09-15 23:24:17 +0900

    Update stock reducer
    * Add action CHANGE_KEY, SELECT_ITEM
    * Add changeKey, selectItem
    * Update fetchStockList with fetch API
    * Add reducer logic with CHANGE_KEY, SELECT_ITEM actions

--- src/modules/stock.js
+++ src/modules/stock.js
@@ -1,21 +1,34 @@
-import { handleActions } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 import createRequestThunk from '../lib/createRequestThunk';
 
 // action name
+const CHANGE_KEY = 'stock/CHANGE_KEY';
+const SELECT_ITEM = 'stock/SELECT_ITEM';
 const FETCH_STOCK_LIST = 'stock/FETCH_STOCK_LIST';
 const FETCH_STOCK_LIST_SUCCESS = 'stock/FETCH_STOCK_LIST_SUCCESS';
 
+const fetchMarket = market =>
+  fetch(`http://127.0.0.1:8000/api/stockinfo/${market}/`);
 // action creator
-export const fetchStockList = createRequestThunk(FETCH_STOCK_LIST, '');
+export const changeKey = createAction(CHANGE_KEY, key => key);
+export const selectItem = createAction(SELECT_ITEM, stockName => stockName);
+export const fetchStockList = createRequestThunk(FETCH_STOCK_LIST, fetchMarket);
 
 // initial state
 const initialState = {
+  key: '',
   list: [],
+  selected: {},
 };
 
 // reducer
 const reducer = handleActions(
   {
+    [CHANGE_KEY]: (state, { payload: key }) => ({ ...state, key }),
+    [SELECT_ITEM]: (state, { payload: stockName }) => ({
+      ...state,
+      selected: state.list.find(item => item.stockName === stockName),
+    }),
     [FETCH_STOCK_LIST_SUCCESS]: (state, { payload: data }) => ({
       ...state,
       list: data,
