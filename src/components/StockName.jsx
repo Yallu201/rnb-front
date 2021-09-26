f6b65e06bdb64ddfe64371f3774c52ed2eb3378a
yallu201 <dlruddlf07@gmail.com>
2021-09-26 10:32:01 +0900

    Optimize rerendering speed with theme changing
    * Remove Button component
    * Button component's attributes( pl, variant, textAlign ) are taking long time to rerender
    * Change Button to Box ( Box component is same as <div> tag ) => https://chakra-ui.com/docs/layout/box
    * Apply useColorModeValue to change hover style option => https://chakra-ui.com/docs/layout/box

--- src/components/StockName.jsx
+++ src/components/StockName.jsx
@@ -1,26 +1,25 @@
-import { Button, Text } from '@chakra-ui/react';
+import { Box, Text, useColorModeValue } from '@chakra-ui/react';
 import { useCallback, memo } from 'react';
 import { useDispatch } from 'react-redux';
 import { selectItem, fetchStockDetail } from '../modules/stock';
 
 const StockName = ({ bookmark, stockCode, stockName }) => {
+  const isDark = useColorModeValue(false, true);
   const dispatch = useDispatch();
   const onClick = useCallback(() => {
     dispatch(selectItem(stockName));
     dispatch(fetchStockDetail(stockCode));
   }, []);
+  const hoverColor = `hover:bg-gray-${isDark ? 500 : 200}`;
   return (
-    <Button
-      pl={4}
-      variant="unstyled"
-      textAlign="left"
+    <Box
+      className={`p-2 pl-4 mb-1 cursor-pointer rounded ${hoverColor}`}
       onClick={onClick}
-      isFullWidth
     >
       <Text isTruncated>
         {stockCode} {stockName}
       </Text>
-    </Button>
+    </Box>
   );
 };
 
