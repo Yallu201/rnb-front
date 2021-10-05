874e685038ee14d6a6e640213ed87ea0c8c78db3
yallu201 <dlruddlf07@gmail.com>
2021-10-05 22:02:31 +0900

    Remove none using reducer & data

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,6 @@
 // Combine reducers & Export rootReducer
 import { combineReducers } from 'redux';
-import sample from './sample.js';
 import stock from './stock.js';
 import loading from './loading.js';
-const rootReducer = combineReducers({ sample, stock, loading });
+const rootReducer = combineReducers({ stock, loading });
 export default rootReducer;
