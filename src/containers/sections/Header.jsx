c67db2d863d09c163c0bbdb59e0a85ff4b903597
yallu201 <dlruddlf07@gmail.com>
2021-10-17 23:50:01 +0900

    Apply useSession hook & Add logout button that be toggled with token

--- src/containers/sections/Header.jsx
+++ src/containers/sections/Header.jsx
@@ -1,18 +1,39 @@
-import { useCallback } from 'react';
-import { Heading, Button } from '@chakra-ui/react';
+import { useCallback, useEffect } from 'react';
+import { Heading, Button, Text, Flex } from '@chakra-ui/react';
 import { Link, useHistory } from 'react-router-dom';
 import { ColorModeSwitcher } from '../../ColorModeSwitcher';
+import { useSession } from '../../hooks';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../modules/auth';
 
 const Header = () => {
   const history = useHistory();
-  const onClickLogin = useCallback(() => history.push('/login'), [history]);
+  const dispatch = useDispatch();
+  const isLogin = useSelector(_ => _.auth.isLogin);
+  const [token, username, updateSessionInfo] = useSession();
+  useEffect(() => {
+    updateSessionInfo();
+  }, [isLogin]);
+  const onClickLogin = useCallback(() => history.push('/login'), []);
+  const onClickLogout = useCallback(() => {
+    dispatch(logout(false));
+    history.push('/');
+  }, []);
   return (
     <div className="container mx-auto p-4 flex">
       <Heading className="flex-grow" size="4xl">
         <Link to="/">RnB</Link>
       </Heading>
       <div className="flex items-center">
-        <Button onClick={onClickLogin}>로그인</Button>
+        {isLogin && (
+          <Flex align="center">
+            <Text mr="2" fontSize="lg">
+              {username} 님
+            </Text>
+            <Button onClick={onClickLogout}>로그아웃</Button>
+          </Flex>
+        )}
+        {!isLogin && <Button onClick={onClickLogin}>로그인</Button>}
         <ColorModeSwitcher />
       </div>
     </div>
