9ec8540eb5060f2c1a33a1606a761e53fd9691ff
kyounghoon <kkh2826@naver.com>
2021-10-31 10:33:05 +0900

    Apply LogoutModal in Header.jsx

--- src/containers/sections/Header.jsx
+++ src/containers/sections/Header.jsx
@@ -3,22 +3,20 @@ import { Heading, Button, Text, Flex } from '@chakra-ui/react';
 import { Link, useHistory } from 'react-router-dom';
 import { ColorModeSwitcher } from '../../ColorModeSwitcher';
 import { useSession } from '../../hooks';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../../modules/auth';
+import { useSelector } from 'react-redux';
+//import { logout } from '../../modules/auth';
+import LogoutModal from '../../pages/Modal/LogoutModal';
+
 
 const Header = () => {
   const history = useHistory();
-  const dispatch = useDispatch();
   const isLogin = useSelector(_ => _.auth.isLogin);
   const [token, username, updateSessionInfo] = useSession();
   useEffect(() => {
     updateSessionInfo();
   }, [isLogin]);
   const onClickLogin = useCallback(() => history.push('/login'), []);
-  const onClickLogout = useCallback(() => {
-    dispatch(logout(false));
-    history.push('/');
-  }, []);
+
   return (
     <div className="container mx-auto p-4 flex">
       <Heading className="flex-grow" size="4xl">
@@ -30,7 +28,8 @@ const Header = () => {
             <Text mr="2" fontSize="lg">
               {username} 님
             </Text>
-            <Button onClick={onClickLogout}>로그아웃</Button>
+            {/* <Button onClick={onClickLogout}>로그아웃</Button> */}
+            <LogoutModal />
           </Flex>
         )}
         {!isLogin && <Button onClick={onClickLogin}>로그인</Button>}
