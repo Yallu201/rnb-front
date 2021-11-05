cd2463a6fece9137c936b18a38f4079d2ca5e2c8
이경일 <leeki19@koreaap.com>
2021-11-05 18:04:50 +0900

    Alternate stock.list to stock.searchList & Add currentPage initialize logic & Apply useCallback to pagenate function

--- src/containers/lists/StockNameList.jsx
+++ src/containers/lists/StockNameList.jsx
@@ -1,39 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector} from 'react-redux';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import Post from './Post';
 import Pagination from './Pagination';
 
 const StockNameList = () => {
-  const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1); //현재 페이지
   const [postPerPage] = useState(15); //페이지당 포스트 개수
-  
-  const list = useSelector(_ => _.stock.list);
+  const searchList = useSelector(_ => _.stock.searchList);
+  const [currentPosts, setCurrentPosts] = useState(searchList);
 
   useEffect(() => {
-    const fetchPosts = () => {
-      setLoading(true);
-      setPosts(list);
-      setLoading(false);
-    }
-    fetchPosts();
-  }, []);
+    //검색 목록이 갱신되었을경우, 페이지 초기화
+    setCurrentPage(1);
+  }, [searchList]);
 
-  //현재페이지 가져오기
-  const indexOfLastPost = currentPage * postPerPage;
-  const indexOfFirstPost = indexOfLastPost - postPerPage;
-  const currentPosts = list.slice(indexOfFirstPost, indexOfLastPost);
-  
-  //클릭 이벤트 페이지 바꾸기 
-  const paginate = pageNum => setCurrentPage(pageNum);
+  useEffect(() => {
+    //현재페이지 가져오기
+    const indexOfLastPost = currentPage * postPerPage;
+    const indexOfFirstPost = indexOfLastPost - postPerPage;
+    const currentPosts_ = searchList.slice(indexOfFirstPost, indexOfLastPost);
+    setCurrentPosts(currentPosts_);
+  }, [searchList, currentPage, postPerPage]);
+
+  //클릭 이벤트 페이지 바꾸기
+  const paginate = useCallback(pageNum => setCurrentPage(pageNum), []);
 
   return (
     <div>
       <Post posts={currentPosts} loading={loading} />
       <Pagination
         postPerPage={postPerPage}
-        totalPosts={list.length}
+        totalPosts={searchList.length}
         paginate={paginate}
       />
     </div>
