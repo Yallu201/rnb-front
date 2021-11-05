0e8fb599b14c04938861e706c7932f42b584cd64
이경일 <leeki19@koreaap.com>
2021-11-05 17:44:43 +0900

    Add action 'FETCH_STOCK_BASICPRICE' & Add state 'searchList', 'basicPrice' & Change reducer 'CHANGE_KEY'

--- src/modules/stock.js
+++ src/modules/stock.js
@@ -9,6 +9,8 @@ const FETCH_STOCK_LIST = 'stock/FETCH_STOCK_LIST';
 const FETCH_STOCK_LIST_SUCCESS = 'stock/FETCH_STOCK_LIST_SUCCESS';
 const FETCH_STOCK_DETAIL = 'stock/FETCH_STOCK_DETAIL';
 const FETCH_STOCK_DETAIL_SUCCESS = 'stock/FETCH_STOCK_DETAIL_SUCCESS';
+const FETCH_STOCK_BASICPRICE = 'stock/FETCH_STOCK_BASICPRICE';
+const FETCH_STOCK_BASICPRICE_SUCCESS = 'stock/FETCH_STOCK_BASICPRICE_SUCCESS';
 
 const fetchMarket = market =>
   fetch(`http://127.0.0.1:8000/api/stockinfo/${market}/`).catch(e =>
@@ -17,6 +19,10 @@ const fetchMarket = market =>
 const fetchDetail = stockCode =>
   fetch(
     `http://127.0.0.1:8000/api/stockinfo/searchdetailinfo/${stockCode}/`
+  ).catch(e => console.error(e));
+const fetchBasic = stockCode =>
+  fetch(
+    `http://127.0.0.1:8000/api/stockinfo/searchcurrentprice/${stockCode}/`
   ).catch(e => console.error(e));
 // action creator
 export const changeKey = createAction(CHANGE_KEY, key => key);
@@ -30,11 +36,16 @@ export const fetchStockDetail = createRequestThunk(
   FETCH_STOCK_DETAIL,
   fetchDetail
 );
+export const fetchStockBasic = createRequestThunk(
+  FETCH_STOCK_BASICPRICE,
+  fetchBasic
+)
 
 // initial state
 const initialState = {
   key: '',
   list: [],
+  searchList: [],
   selected: {
     stockCode: '종목을 선택하세요',
     stockName: '',
@@ -47,12 +58,17 @@ const initialState = {
     TENYEARS: [],
     THREEMONTH: [],
   },
+  priceBasic: {}
 };
 
 // reducer
 const reducer = handleActions(
   {
-    [CHANGE_KEY]: (state, { payload: key }) => ({ ...state, key }),
+    [CHANGE_KEY]: (state, { payload: key }) => ({ 
+      ...state, 
+      key: key,
+      searchList: state.list.filter(item => item.stockName.toLowerCase().includes(key.toLowerCase())) 
+    }),
     [SELECT_ITEM]: (state, { payload: stockName }) => ({
       ...state,
       selected: state.list.find(item => item.stockName === stockName),
@@ -64,12 +80,17 @@ const reducer = handleActions(
     [FETCH_STOCK_LIST_SUCCESS]: (state, { payload: data }) => ({
       ...state,
       list: data,
+      searchList: data,
     }),
     [FETCH_STOCK_DETAIL_SUCCESS]: (state, { payload: detail }) => ({
       ...state,
       duration: 'ONEMONTH',
       detail,
     }),
+    [FETCH_STOCK_BASICPRICE_SUCCESS]: (state, { payload: data }) => ({
+      ...state,
+      priceBasic: data
+    })
   },
   initialState
 );
