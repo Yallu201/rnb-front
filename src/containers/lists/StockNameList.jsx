dfc85696de2df86c49f67993e5591bff24e5c478
이경일 <leeki19@koreaap.com>
2021-11-08 15:42:34 +0900

    Create pagination without react-paginate

--- src/containers/lists/StockNameList.jsx
+++ src/containers/lists/StockNameList.jsx
@@ -4,33 +4,24 @@ import Post from './Post';
 import Pagination from './Pagination';
 
 const StockNameList = () => {
-  const [currentPage, setCurrentPage] = useState(1); //현재 페이지
-  const [postPerPage] = useState(15); //페이지당 포스트 개수
+  const itemsPerPage = useSelector(_ => _.stock.pagenation.itemsPerPage); //페이지당 포스트 개수
   const searchList = useSelector(_ => _.stock.searchList);
+  const currentPage = useSelector(_ => _.stock.pagenation.currentPage);
   const [currentPosts, setCurrentPosts] = useState(searchList);
 
   useEffect(() => {
-    //검색 목록이 갱신되었을경우, 페이지 초기화
-    setCurrentPage(1);
-  }, [searchList]);
-
-  useEffect(() => {
     //현재페이지 가져오기
-    const indexOfLastPost = currentPage * postPerPage;
-    const indexOfFirstPost = indexOfLastPost - postPerPage;
+    const indexOfLastPost = currentPage * itemsPerPage;
+    const indexOfFirstPost = indexOfLastPost - itemsPerPage;
     const currentPosts_ = searchList.slice(indexOfFirstPost, indexOfLastPost);
     setCurrentPosts(currentPosts_);
-  }, [searchList, currentPage, postPerPage]);
+  }, [searchList, currentPage, itemsPerPage]);
 
   return (
-    <div>
+    <>
       <Post posts={currentPosts} />
-      <Pagination
-        postPerPage={postPerPage}
-        totalPosts={searchList.length}
-        paginate={setCurrentPage}
-      />
-    </div>
+      <Pagination />
+    </>
   );
 };
 
