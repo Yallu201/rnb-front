1f2941a7c566f79cda6d464a4ebc6b003317d73c
이경일 <leeki19@koreaap.com>
2021-11-09 15:36:06 +0900

    Apply LogoutModal to Header component

--- src/containers/sections/Header.jsx
+++ src/containers/sections/Header.jsx
@@ -1,28 +1,24 @@
-import React, { useCallback, useEffect } from 'react';import { Heading, Button, Text, Flex } from '@chakra-ui/react';
+import React, { useCallback, useEffect } from 'react';
+import { Heading, Button, Text, Flex } from '@chakra-ui/react';
 import { Link, useHistory } from 'react-router-dom';
 import { ColorModeSwitcher } from '../../ColorModeSwitcher';
 import { useSession } from '../../hooks';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../../modules/auth';
-import BookmarkDrawing  from '../../components/Drawer/BookmarkDrawing';
+import { useSelector } from 'react-redux';
+import BookmarkDrawing from '../../components/Drawer/BookmarkDrawing';
 import UserInfoDrawing from '../../components/Drawer/UserInfoDrawing';
+import LogoutModal from '../../components/Modal/LogoutModal';
 
 const Header = () => {
   const history = useHistory();
-  const dispatch = useDispatch();
 
   const isLogin = useSelector(_ => _.auth.isLogin);
   const [token, username, updateSessionInfo] = useSession();
-  
+
   useEffect(() => {
     updateSessionInfo();
   }, [isLogin]);
-  
+
   const onClickLogin = useCallback(() => history.push('/login'), []);
-  const onClickLogout = useCallback(() => {
-    dispatch(logout(false));
-    history.push('/');
-  }, []);
 
   return (
     <div className="container mx-auto p-4 flex">
@@ -35,7 +31,7 @@ const Header = () => {
             <Text mr="2" fontSize="lg">
               {username} 님
             </Text>
-            <Button onClick={onClickLogout}>로그아웃</Button>
+            <LogoutModal />
           </Flex>
         )}
         {!isLogin && <Button onClick={onClickLogin}>로그인</Button>}
