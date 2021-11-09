04c90da1d0579abdb7d6308a1a77372ff3038ed4
이경일 <leeki19@koreaap.com>
2021-11-09 17:33:14 +0900

    Remove grid-rows-2 className from MainBoard component

--- src/pages/MainBoard.jsx
+++ src/pages/MainBoard.jsx
@@ -18,7 +18,7 @@ const MainBoard = () => {
   });
   return (
     <div className="grid grid-cols-3 gap-4 h-full">
-      <div className="col-span-2 grid grid-rows-2 gap-4 ">
+      <div className="col-span-2 grid gap-4">
         <StockDetail />
       </div>
       <div className="col-span-1 h-full">
