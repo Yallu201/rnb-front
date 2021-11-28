31b9f2a1136c5f5340613123230c971362c8f96a
yallu201 <dlruddlf07@gmail.com>
2021-11-29 00:34:00 +0900

    Alternate isStar to 'bookmark' in StockName component

--- src/components/StockName.jsx
+++ src/components/StockName.jsx
@@ -2,35 +2,28 @@ import { Box, Flex, useColorModeValue } from '@chakra-ui/react';
 import { useCallback, memo, useState } from 'react';
 import { StarIcon } from '@chakra-ui/icons';
 import { useMount } from '../hooks';
+import { toggleBookmark } from '../modules/stock';
+import { useDispatch } from 'react-redux';
 
-const StockName = ({ bookmark = false, stockName, stockCode, onClick }) => {
+const StockName = ({
+  bookmark = false,
+  stockName,
+  stockCode,
+  onClick = () => {},
+}) => {
+  const dispatch = useDispatch();
   const isDark = useColorModeValue(false, true);
   const hoverColor = `hover:bg-gray-${isDark ? 500 : 200}`;
-  const [isStar, setIsStar] = useState(bookmark);
-  useMount(() => {
-    const stars = localStorage.getItem('stars') || ''; // 값이 없으면 빈 문자열로 초기화
-    const token = `,${stockCode}`; // ',' 쉼표로 구분자 사용
-    // #1 - bookmark 값이 서버에서 전달되었을경우
-    if (isStar) {
-      const stars_ = stars.replace(token, '');
-      localStorage.setItem('stars', stars_.concat(token));
-      return;
-    }
-    // #2 - localStorage에 stockCode가 저장된 경우
-    if (stars.includes(token)) {
-      setIsStar(true);
-    }
-  });
   const onClickStar = useCallback(
     e => {
       e.stopPropagation(); // 상위 Element로 이벤트 전파 차단( 버블링, 캡쳐링 )
       const stars = localStorage.getItem('stars') || '';
       const token = `,${stockCode}`;
       const stars_ = stars.replace(token, '');
-      localStorage.setItem('stars', stars_.concat(isStar ? '' : token));
-      setIsStar(!isStar);
+      localStorage.setItem('stars', stars_.concat(bookmark ? '' : token));
+      dispatch(toggleBookmark({ stockCode, bookmark }));
     },
-    [isStar]
+    [bookmark]
   );
   return (
     <Box
@@ -43,7 +36,7 @@ const StockName = ({ bookmark = false, stockName, stockCode, onClick }) => {
           <span className="pl-5">{stockName}</span>
         </Box>
         <StarIcon
-          color={`${isStar ? 'yellow' : 'gray'}.400`}
+          color={`${bookmark ? 'yellow' : 'gray'}.400`}
           onClick={onClickStar}
         />
       </Flex>
